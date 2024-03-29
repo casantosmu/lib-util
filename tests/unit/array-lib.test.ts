52656a6617120ae1121a2b8a997cffd087a81e5a
Carlos Santos <casantosmu@gmail.com>
2024-03-29 01:01:21 +0100

    Add test cases for empty slots

--- tests/unit/array-lib.test.ts
+++ tests/unit/array-lib.test.ts
@@ -34,6 +34,19 @@ describe("ArrayLib", () => {
         expect(result).toThrow(RangeError);
       });
     });
+
+    describe("with an index where the element has been deleted", () => {
+      test("throws a RangeError", () => {
+        const arrayLib = new ArrayLib(["a", "b"], Type.String);
+        delete arrayLib[0];
+
+        const result = (): void => {
+          arrayLib.elementAt(0);
+        };
+
+        expect(result).toThrow(RangeError);
+      });
+    });
   });
 
   describe("first method", () => {
@@ -44,6 +57,17 @@ describe("ArrayLib", () => {
         const result = arrayLib.first();
 
         expect(result).toBe("a");
+      });
+    });
+
+    describe("in an ArrayLib instance where first element has been deleted", () => {
+      test("returns next element counting from the removed element", () => {
+        const arrayLib = new ArrayLib(["a", "b"], Type.String);
+        delete arrayLib[0];
+
+        const result = arrayLib.first();
+
+        expect(result).toBe("b");
       });
     });
 
